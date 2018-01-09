9f6b52c52f56ee275a5d3f2c6317db9bbcd8423c
Jummit <jummit@web.de>
2018-01-09 09:19:40 +0100

    formated utils.lua

--- engine/utils.lua
+++ engine/utils.lua
@@ -1,7 +1,6 @@
-local utils = {}
-utils.sleep = function(time)
-  local endTime = os.clock() + time
-  repeat until os.clock() > endTime
-end
-
-return utils
+return {
+  sleep = function(time)
+    local endTime = os.clock() + time
+    repeat until os.clock() > endTime
+  end
+}
