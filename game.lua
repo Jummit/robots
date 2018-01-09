531680385bfe9156537f8f04a6e0641d948f9a4b
Jummit <jummit@web.de>
2018-01-09 09:20:53 +0100

    renamed variables in game

--- game.lua
+++ game.lua
@@ -1,16 +1,16 @@
-local world = require "Documents/Programming/Lua/Love/Robots/engine/world"
+local worldEngine = require "Documents/Programming/Lua/Love/Robots/engine/world"
 local utils = require "Documents/Programming/Lua/Love/Robots/engine/utils"
-local script = require "Documents/Programming/Lua/Love/Robots/engine/script"
+local scriptEngine = require "Documents/Programming/Lua/Love/Robots/engine/script"
 local tiles = require "Documents/Programming/Lua/Love/Robots/engine/tiles"
 
 --make new world and draw it
-local newWorld = world.new(80, 20, 2040192, tiles)
+local world = worldEngine.new(80, 20, 2040192, tiles)
 
-newWorld:init(10, 10)
+world:init(10, 10)
 while true do
-  newWorld:draw()
+  world:draw()
   utils.sleep(0.5)
 
-  script.fillEventQueues(newWorld)
-  script.runEventQuenues(newWorld)
+  scriptEngine.fillEventQueues(newWorld)
+  scriptEngine.runEventQuenues(newWorld)
 end
