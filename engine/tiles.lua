757d1d41405e7b6b87d87d2520d2e63f2aac6d2d
Jummit <jummit@web.de>
2018-01-09 09:19:15 +0100

    moved tile table to file

---
+++ engine/tiles.lua
@@ -0,0 +1,14 @@
+return {
+  ground = {
+    name = "ground",
+    char = {".", ",", "¸"}
+  },
+  stone = {
+    name = "stone",
+    char = {"o", "O", "0", "Q"}
+  },
+  machine = {
+    name = "machine",
+    char = {"H"}
+  }
+}
--- game.lua
+++ game.lua
@@ -1,25 +1,10 @@
 local world = require "Documents/Programming/Lua/Love/Robots/engine/world"
 local utils = require "Documents/Programming/Lua/Love/Robots/engine/utils"
 local script = require "Documents/Programming/Lua/Love/Robots/engine/script"
+local tiles = require "Documents/Programming/Lua/Love/Robots/engine/tiles"
 
 --make new world and draw it
-local newWorld = world.new(
-  80, 20, 2040192,
-  {
-    ground = {
-      name = "ground",
-      char = {".", ",", "¸"}
-    },
-    stone = {
-      name = "stone",
-      char = {"o", "O", "0", "Q"}
-    },
-    machine = {
-      name = "machine",
-      char = {"H"}
-    }
-  }
-)
+local newWorld = world.new(80, 20, 2040192, tiles)
 
 newWorld:init(10, 10)
 while true do
